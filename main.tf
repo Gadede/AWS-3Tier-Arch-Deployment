f3998b81de8ad7073ee741af46ec22ca92c3e3cc
Richy <richlovesoglo@gmail.com>
2026-06-17 08:19:36 +0800

    commented out s3 bucket resources

--- main.tf
+++ main.tf
@@ -64,38 +64,38 @@ module "compute" {
   db_name                  = "myappdb"
 }
 
-# Create S3 bucket 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "richkode-tf-state-bucket"
+# # Create S3 bucket 
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "richkode-tf-state-bucket"
 
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
-}
+#   # lifecycle {
+#   #   prevent_destroy = true
+#   # }
+# }
 
-# Enable Versioning
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# # Enable Versioning
+# resource "aws_s3_bucket_versioning" "terraform_state" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-# Enable server side encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+# # Enable server side encryption
+# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
-# Block public access to the bucket
-resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket                  = aws_s3_bucket.terraform_state.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+# # Block public access to the bucket
+# resource "aws_s3_bucket_public_access_block" "terraform_state" {
+#   bucket                  = aws_s3_bucket.terraform_state.id
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
