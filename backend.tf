6582736687ef0b18f23d955f108c503938423336
Richy <richlovesoglo@gmail.com>
2026-06-24 01:41:49 +0800

    Redeploy Code

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "richkode-terraform-state-bucket"
+    bucket       = "richkode-terraformstate-bucket"
     key          = "dev/terraform.tfstate"
     region       = "us-east-2"
     encrypt      = true
